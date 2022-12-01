c77abe10ee7fad6e9bfd0795a4c5c1108d6bc1ed
MigatteNoGokuii <anmar_2101@hotmail.com>
2022-12-01 11:42:27 +0400

    Tweeking Combat system(Undone) & Transferring To PC

---
+++ Assets/SwordAttack.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       /* if(other.GetComponent<DummyEnemy>() != null)
+        {
+            other.GetComponent<DummyEnemy>().hitted = true;
+            other.GetComponent<DummyEnemy>().TakeDamage();
+        }
+       */
+    }
+}
