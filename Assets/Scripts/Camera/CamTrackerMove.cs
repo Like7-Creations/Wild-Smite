893647859b813d30a324d877bd9847a1a4b304c9
DanjerSnekker <viperrules4ever@gmail.com>
2023-03-07 17:05:07 +0400

    Added the Arrival Steering Behavior to the Cam Tracker

--- Assets/Scripts/Camera/CamTrackerMove.cs
+++ Assets/Scripts/Camera/CamTrackerMove.cs
@@ -5,27 +5,64 @@ using UnityEngine.Animations;
 
 public class CamTrackerMove : MonoBehaviour
 {
-    public GameObject target;
-    public Dynamic_SplitScreen dSplitScreen;
-
-    public Vector3 splitOffset;
-
-    public bool splitScreen;
+    #region Target Settings
+    public GameObject playerTarget;
 
     [SerializeField] Vector3 targetOffset;
+    Vector3 targetPos;
+    #endregion
 
-    //[SerializeField] float maxDistFromTarget;
+    #region SplitScreen Settings
+    public Dynamic_SplitScreen dSplitScreen;
+    public Vector3 splitOffset;
+    public bool splitScreen;
+    #endregion
 
-    [SerializeField] float baseTrackerSpeed;
-    [SerializeField] float trackerAcceleration;
-
-
-
+    #region PlayerDesignation
     bool isP1;
     bool isP2;
+    #endregion
 
+    #region Arrival Behavior Settings
+    public float trackerMass = 15;
+    public float tracker_MaxVel = 3;
+    public float tracker_MaxForce = 15;
+    public float slowRadius;
+
+    Vector3 velocity;
+    #endregion
+
+    Vector3 Arrival()
+    {
+        Vector3 desiredVel = targetPos - transform.position;
+        desiredVel = desiredVel.normalized * tracker_MaxVel;
+
+        float dist = Vector3.Distance(targetPos, transform.position);
+
+        if(dist<slowRadius)
+        {
+            desiredVel = desiredVel.normalized * tracker_MaxVel * (dist / slowRadius);
+
+        }
+        else
+        {
+            desiredVel = desiredVel.normalized * tracker_MaxVel;
+        }
+
+        Vector3 arrival = desiredVel - velocity;
+        arrival = Vector3.ClampMagnitude(arrival, tracker_MaxVel);
+        arrival /= trackerMass;
+
+        velocity = Vector3.ClampMagnitude(velocity + arrival, tracker_MaxVel);
+
+        return velocity * Time.deltaTime;
+    }
+
+    #region Standard Functions
     void Start()
     {
+        velocity = Vector3.zero;
+
         dSplitScreen = FindObjectOfType<Dynamic_SplitScreen>();
 
 
@@ -61,16 +98,22 @@ public class CamTrackerMove : MonoBehaviour
             splitOffset = Vector3.zero;
         }
 
+        targetPos = playerTarget.transform.position - (-splitOffset + targetOffset);
+
+        transform.position += Arrival();
+
+        #region Failed Attempts
         /*if(Vector3.Distance(transform.position, target.transform.position) > maxDistFromTarget)
         {
             PlayerStats stats = target.GetComponent<PlayerStats>();
 
             trackerAcceleration = 
-        }*/
+        }
 
         transform.position = Vector3.Lerp(transform.position, 
                                         target.transform.position - (-splitOffset + targetOffset),
-                                        baseTrackerSpeed * Time.deltaTime);
+                                        baseTrackerSpeed * Time.deltaTime);*/
+        #endregion
     }
 
     void OnDrawGizmos()
@@ -78,4 +121,5 @@ public class CamTrackerMove : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(transform.position, 1f);
     }
+    #endregion
 }
