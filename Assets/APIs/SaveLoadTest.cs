69377a6530e4c9d932fbb93e6d4660607ace1c03
MigatteNoGokuii <anmar_2101@hotmail.com>
2023-04-11 16:15:49 +0400

    Implemented Save & Load slot system

--- Assets/APIs/SaveLoadTest.cs
+++ Assets/APIs/SaveLoadTest.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Serielization;
 using TMPro;
+using UnityEngine.UI;
 
 public class SaveLoadTest : MonoBehaviour
 {
-    public string lol;
-    public string lolTwo;
-    public TextMeshProUGUI lolly;
-    public TextMeshProUGUI lollyTwo;
+    public string slot;
+    public string saveName;
 
     public PlayerStat_Data dataToSave;
     public PlayerStat_Data dataToLoad;
 
+    public Button save1;
+    public Button save2;
+    public Button save3;
+
     void Start()
     {
-        
+        LoadSlots();
     }
 
     // Update is called once per frame
@@ -32,24 +35,72 @@ public class SaveLoadTest : MonoBehaviour
             Debug.Log("loaded");
             Load();
         }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("loaded");
+            LoadSlots();
+        }
     }
 
     void Save()
     {
-        SaveLoadSystem.BeginSave("/player.data");
+        SaveLoadSystem.BeginSave(slot);
+        SaveLoadSystem.Insert(saveName);
         PlayerData playerdata = new PlayerData(dataToSave.hp, dataToSave.stamina, dataToSave.m_ATK, dataToSave.r_ATK, dataToSave.playerIndex, dataToSave.current_XP, dataToSave.lvl);
         SaveLoadSystem.Insert(playerdata);
-        //SaveLoadSystem.Insert(lolTwo);
         SaveLoadSystem.EndSave();
     }
 
     void Load()
     {
-        SaveLoadSystem.BeginLoad("/player.data");
+        SaveLoadSystem.BeginLoad(slot);
         PlayerData playerdata = SaveLoadSystem.Load<PlayerData>();
         dataToLoad.LoadStats(playerdata.hp, playerdata.stamina, playerdata.melee, playerdata.range, playerdata.index, playerdata.currentXP, playerdata.level);
-        //lollyTwo.text = SaveLoadSystem.Load<string>();
         SaveLoadSystem.EndLoad();
+    }
+
+    public void LoadSlots()
+    {
+        SaveLoadSystem.BeginLoad("/SlotOne.data");
+        save1.GetComponentInChildren<TextMeshProUGUI>().text = SaveLoadSystem.Load<string>();
+        SaveLoadSystem.EndLoad();
+
+        SaveLoadSystem.BeginLoad("/SlotTwo.data");
+        save2.GetComponentInChildren<TextMeshProUGUI>().text = SaveLoadSystem.Load<string>();
+        SaveLoadSystem.EndLoad();
+
+        SaveLoadSystem.BeginLoad("/SlotThree.data");
+        save3.GetComponentInChildren<TextMeshProUGUI>().text = SaveLoadSystem.Load<string>();
+        SaveLoadSystem.EndLoad();
+
+        if(save1.GetComponentInChildren<TextMeshProUGUI>().text == null)
+        {
+            save1.GetComponentInChildren<TextMeshProUGUI>().text = "No Save Slot";
+        }
+        if (save2.GetComponentInChildren<TextMeshProUGUI>().text == null)
+        {
+            save2.GetComponentInChildren<TextMeshProUGUI>().text = "No Save Slot";
+        }
+        if (save3.GetComponentInChildren<TextMeshProUGUI>().text == null)
+        {
+            save3.GetComponentInChildren<TextMeshProUGUI>().text = "No Save Slot";
+        }
+    }
+
+    public void SlotOne()
+    {
+        slot = "/SlotOne.data";
+        saveName = "farhans's Save";
+    }
+    public void SlotTwo()
+    {
+        slot = "/SlotTwo.data";
+        saveName = "chris's Save";
+    }
+    public void SlotThree()
+    {
+        slot = "/SlotThree.data";
+        saveName = "anmar's Save";
     }
 }
 
@@ -74,4 +125,10 @@ public class PlayerData
         this.currentXP = currentXP;
         this.level = level;
     }
+}
+
+[System.Serializable]
+public class SaveSlot
+{
+    string name;
 }
