99ea08195d5a78ae1dddfd543a40d5964cf5d453
AnmarAlyousef <anmar@hypemaster.com>
2024-01-19 11:33:08 +0400

    Fix Editor Script Error

--- Assets/Scripts/Enemies/AttackTypes/AttackEditor.cs
+++ Assets/Scripts/Enemies/AttackTypes/AttackEditor.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEditor;
-
 #if (UNITY_EDITOR)
+using UnityEditor;
+
 [CustomEditor(typeof(Attack), true)]
 public class AttackEditor : Editor
-{
-    
+{   
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
